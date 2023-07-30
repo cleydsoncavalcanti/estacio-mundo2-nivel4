347b53a17a79f2946eaf0fce884bd053e4a5da28
Cavalcanti <cleydsoncavalcanti@gmail.com>
2023-07-30 12:51:43 -0300

    ✨ criando livro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { ControleLivrosService } from './controle-livros.service';
     LivroDadosComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
